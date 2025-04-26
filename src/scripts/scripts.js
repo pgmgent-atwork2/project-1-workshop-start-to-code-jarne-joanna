bf6e82b6129084fee403edb84bf3f0fc2cdbc22a
«JarneTek» <jarnteki@student.arteveldehs.be>
2025-04-26 19:10:45 +0200

    changed some const for beter reading

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,19 +1,19 @@
 // selecting all the elements from the homepage
-const wordDisplay = document.querySelector(".word-display");
+const displayText = document.querySelector(".word-display");
 const hangmanImage = document.querySelector(".hangman-box img");
-const gameModal = document.querySelector(".game-modal");
+const endGamePopup = document.querySelector(".game-modal");
 const playAgainButton = document.querySelector("button");
 const keyboardDiv = document.querySelector(".keyboard");
 const guessesText = document.querySelector(".guesses-text b");
 
 // Initializing the game variables
 let currentWord, correctLetters, wrongGuessCount;
-const maxGuesses = 6;
+const maxGuessCount = 6;
 
 // Function to display the end screen when the game is over
-function showGameOver(isVictory) {
+function showGameOver(isWinner) {
   // Determine what content to show based on win/loss
-  if (isVictory) {
+  if (isWinner) {
     modalText = "You found the word!";
     imageSource = "./src/images/victory.gif";
     headingText = "Congratulations!";
@@ -24,13 +24,13 @@ function showGameOver(isVictory) {
   }
 
   // Update the modal elements with the appropriate content
-  gameModal.querySelector("img").src = imageSource;
-  gameModal.querySelector("h4").innerText = headingText;
+  endGamePopup.querySelector("img").src = imageSource;
+  endGamePopup.querySelector("h4").innerText = headingText;
   // Display the correct word in the modal
-  gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+  endGamePopup.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
 
   // Make the modal visible
-  gameModal.classList.add("show");
+  endGamePopup.classList.add("show");
 }
 
 // Function to start a new game
@@ -38,10 +38,10 @@ const resetGame = () => {
   correctLetters = [];
   hangmanImage.src = `./src/images/hangman-0.svg`;
   wrongGuessCount = 0;
-  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuessCount}`;
 
   // creates the empty letter slots (spliting the word into an array of letters and mapping them to a list item)
-  wordDisplay.innerHTML = currentWord
+  displayText.innerHTML = currentWord
     .split("")
     .map(() => `<li class="letter"></li>`)
     .join("");
@@ -52,10 +52,10 @@ const resetGame = () => {
     .forEach((btn) => (btn.disabled = false));
 
   // hide the game modal
-  gameModal.classList.remove("show");
+  endGamePopup.classList.remove("show");
 };
 
-//creating a for loop to display our keyboard buttons
+//creating a for loop to display our keyboard buttons we use the numbers 97 to 122 since these are the ASCII values for the lowercase letters a-z
 for (let i = 97; i <= 122; i++) {
   //creating a button for each letter of the alphabet
   const button = document.createElement("button");
@@ -65,45 +65,40 @@ for (let i = 97; i <= 122; i++) {
   keyboardDiv.appendChild(button);
   //adding an event listener to the button fromcharcode makes sure that we get the correct letter when we click on the button
   button.addEventListener("click", (e) =>
-    initGame(e.target, String.fromCharCode(i))
+    startGame(e.target, String.fromCharCode(i))
   );
 }
 
 //function to handle the game logic when a button on the keyboard is clicked
-const initGame = (button, clickedLetter) => {
+const startGame = (button, clickedLetter) => {
   // Check if the letter is in the word
   if (currentWord.includes(clickedLetter)) {
-    // First, split the word into an array of individual letters
-    const letters = currentWord.split('');
-    
-    // Loop through each letter in our array
-    for (const letter of letters) {
-      // Find the position of current letter in the word we use indexof to find the index, this is a built in method in JS method
-      const index = letters.indexOf(letter);
-      
-      // If this letter matches the one clicked
-      if (letter === clickedLetter) {
-        correctLetters.push(letter);
-        const letterSlot = wordDisplay.querySelectorAll("li")[index];
-        
+    // Split word into array of letters
+    const letters = currentWord.split("");
+
+    // Loop through each position in the word
+    for (let index = 0; index < letters.length; index++) {
+      if (letters[index] === clickedLetter) {
+        correctLetters.push(letters[index]);
+        const letterSlot = displayText.querySelectorAll("li")[index];
+
         // Update the display
-        letterSlot.innerText = letter;
+        letterSlot.innerText = letters[index];
         letterSlot.classList.add("guessed");
       }
     }
   } else {
-    //this will update the wrong guess count and the hangman image if the letter is not in the word
-    //wrongGuessCount++; makes sure that we can show all of our images based of the name of our images
+    // Handle wrong guess
     wrongGuessCount++;
     hangmanImage.src = `./src/images/hangman-${wrongGuessCount}.svg`;
   }
   //disable the button after it is clicked
   button.disabled = true;
   //update the guesscount
-  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuessCount}`;
 
   //check if the game is over
-  if (wrongGuessCount === maxGuesses) {
+  if (wrongGuessCount === maxGuessCount) {
     showGameOver(false);
   } else if (
     currentWord.split("").every((letter) => correctLetters.includes(letter))
@@ -114,12 +109,16 @@ const initGame = (button, clickedLetter) => {
 
 // Function to get a random word from our word-list.js
 const getRandomWord = () => {
-  // this will pick a random word from the array of words, this may look a bit weird since we are destructuring the word and hint from the array of objects, but it is a common practice in JS to do this.
-  const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
-  //set the current word and update the hint text
-  currentWord = word;
-  document.querySelector(".hint-text b").innerText = hint;
-  //reset the game
+  // Get random index from the word list
+  const randomIndex = Math.floor(Math.random() * wordList.length);
+  // Get the word object based of the randomized index
+  const wordObject = wordList[randomIndex];
+
+  // Set the current word and hint
+  currentWord = wordObject.word;
+  document.querySelector(".hint-text b").innerText = wordObject.hint;
+
+  // Reset the game
   resetGame();
 };
 
@@ -128,6 +127,6 @@ getRandomWord();
 
 //add an event listener to the play again button
 playAgainButton.addEventListener("click", () => {
-  gameModal.classList.remove("show");
+  endGamePopup.classList.remove("show");
   getRandomWord();
 });
