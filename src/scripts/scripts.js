e2bf423ef67e32215d89a7afd49971b71dbaa749
«JarneTek» <jarnteki@student.arteveldehs.be>
2025-04-28 13:51:30 +0200

    refactor: update HTML structure and CSS styles for improved layout and readability and adjusted some js file syntaxes to be better read and understandeable

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,13 +1,13 @@
-// selecting all the elements from the homepage
-const displayText = document.querySelector(".word-display");
-const hangmanImage = document.querySelector(".hangman-box img");
-const endGamePopup = document.querySelector(".game-modal");
-const playAgainButton = document.querySelector("button");
-const keyboardDiv = document.querySelector(".keyboard");
-const guessesText = document.querySelector(".guesses-text b");
+// Selecting all elements
+const displayText = document.querySelector(".letters");
+const hangmanImage = document.querySelector(".drawing img");
+const endGamePopup = document.querySelector(".popup");
+const playAgainButton = document.querySelector(".restart-btn");
+const keyboardContainer = document.querySelector(".buttons");
+const guessesText = document.querySelector(".score b");
 
 // Initializing the game variables
-let currentWord, correctLetters, wrongGuessCount;
+let selectedWord, correctLetters, wrongGuessCount;
 const maxGuessCount = 6;
 
 // Function to display the end screen when the game is over
@@ -25,9 +25,11 @@ function showGameOver(isWinner) {
 
   // Update the modal elements with the appropriate content
   endGamePopup.querySelector("img").src = imageSource;
-  endGamePopup.querySelector("h4").innerText = headingText;
+  endGamePopup.querySelector("h2").innerText = headingText;
   // Display the correct word in the modal
-  endGamePopup.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+  endGamePopup.querySelector(
+    "p"
+  ).innerHTML = `${modalText} <b>${selectedWord}</b>`;
 
   // Make the modal visible
   endGamePopup.classList.add("show");
@@ -41,13 +43,13 @@ const resetGame = () => {
   guessesText.innerText = `${wrongGuessCount} / ${maxGuessCount}`;
 
   // creates the empty letter slots (spliting the word into an array of letters and mapping them to a list item)
-  displayText.innerHTML = currentWord
+  displayText.innerHTML = selectedWord
     .split("")
     .map(() => `<li class="letter"></li>`)
     .join("");
 
   // enable keyboard buttons
-  keyboardDiv
+  keyboardContainer
     .querySelectorAll("button")
     .forEach((btn) => (btn.disabled = false));
 
@@ -62,7 +64,7 @@ for (let i = 97; i <= 122; i++) {
   // converts ASCII/Unicode values to their corresponding characters
   button.innerText = String.fromCharCode(i);
   //adding the button to the keyboard div
-  keyboardDiv.appendChild(button);
+  keyboardContainer.appendChild(button);
   //adding an event listener to the button fromcharcode makes sure that we get the correct letter when we click on the button
   button.addEventListener("click", (e) =>
     startGame(e.target, String.fromCharCode(i))
@@ -72,9 +74,9 @@ for (let i = 97; i <= 122; i++) {
 //function to handle the game logic when a button on the keyboard is clicked
 const startGame = (button, clickedLetter) => {
   // Check if the letter is in the word
-  if (currentWord.includes(clickedLetter)) {
+  if (selectedWord.includes(clickedLetter)) {
     // Split word into array of letters
-    const letters = currentWord.split("");
+    const letters = selectedWord.split("");
 
     // Loop through each position in the word
     for (let index = 0; index < letters.length; index++) {
@@ -101,7 +103,7 @@ const startGame = (button, clickedLetter) => {
   if (wrongGuessCount === maxGuessCount) {
     showGameOver(false);
   } else if (
-    currentWord.split("").every((letter) => correctLetters.includes(letter))
+    selectedWord.split("").every((letter) => correctLetters.includes(letter))
   ) {
     showGameOver(true);
   }
@@ -115,8 +117,8 @@ const getRandomWord = () => {
   const wordObject = wordList[randomIndex];
 
   // Set the current word and hint
-  currentWord = wordObject.word;
-  document.querySelector(".hint-text b").innerText = wordObject.hint;
+  selectedWord = wordObject.word;
+  document.querySelector(".clue").innerText = wordObject.hint;
 
   // Reset the game
   resetGame();
